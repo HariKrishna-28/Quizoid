73742fbb6a23aed757ae6d0731d79e28c4932aa4
HariKrishna-28 <harikrishnac2002@gmail.com>
2021-08-11 19:54:36 +0530

    refactored navigation bar

--- src/components/Home page/pages/Home.js
+++ src/components/Home page/pages/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { MenuList } from "../helpers/MenuList";
 import MenuItem from "../C/MenuItem";
-import Navbar from '../C/Navbar'
+// import Navbar from '../C/Navbar'
+import Nav from "../C/Nav";
 import { Link } from 'react-router-dom'
 import Footer from "../C/Footer";
 import "../styles/Home.css";
@@ -18,52 +19,54 @@ function Home() {
   // const [submitted, setSubmitted] = useState(false);
 
   return (
-    <div style={{
-      backgroundColor: 'black',
-      marginBottom: 0,
-      marginLeft: 0,
-      marginRight: 0,
-    }}>
-      <Navbar />
+    <>
+      <Nav />
+      <div style={{
+        backgroundColor: 'black',
+        marginBottom: 0,
+        marginLeft: 0,
+        marginRight: 0,
+      }}>
+        {/* <Navbar /> */}
 
-      <div className="home" style={{ objectFit: 'fill', marginTop: 0, marginBottom: 0, }}>
+        <div className="home" style={{ objectFit: 'fill', marginTop: 0, marginBottom: 0, }}>
 
-      </div>
-
-      <div style={{ "backgroundColor": "#ffffff", "backgroundImage": "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='250' height='30' viewBox='0 0 1000 120'%3E%3Cg fill='none' stroke='%23fafafa' stroke-width='10' %3E%3Cpath d='M-500 75c0 0 125-30 250-30S0 75 0 75s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 45c0 0 125-30 250-30S0 45 0 45s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 105c0 0 125-30 250-30S0 105 0 105s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 15c0 0 125-30 250-30S0 15 0 15s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500-15c0 0 125-30 250-30S0-15 0-15s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 135c0 0 125-30 250-30S0 135 0 135s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3C/g%3E%3C/svg%3E\")", textAlign: 'center', color: '#1e1e1e', fontSize: '25px' }}>
-        <h5><br></br></h5>
-
-        <h1 className='pt-2 pb-3'>INSTRUCTIONS</h1>
-        <div className='pb-2 mx-3'>
-
-          <h5> Select the Categories from the <Link to='/categories' style={{ textDecoration: 'none' }}>categories</Link> page.<br></br></h5>
-          <h5> There will be 4 choices for each quiz question.<br></br></h5>
-          <h5>Select the correct option.<br></br></h5>
-          <h5>The right answer will be displayed in <span style={{ color: 'green' }}>Green</span> colour.<br></br></h5>
-          <h5>The wrong answers will be displayed in <span style={{ color: 'red' }}>Red</span> colour.</h5>
-          <h5>Click Next to move to the next question.<br></br></h5>
-          <h5>After 10 questions are completed move to leaderboard to see your scores!<br></br></h5>
-          <h5>If you abandon the quiz, your score will be considered as zero.<br></br></h5>
         </div>
 
-        <div className="menu" style={{ backgroundColor: "#ffffff", paddingBottom: '5rem', "backgroundImage": "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='250' height='30' viewBox='0 0 1000 120'%3E%3Cg fill='none' stroke='%23fafafa' stroke-width='10' %3E%3Cpath d='M-500 75c0 0 125-30 250-30S0 75 0 75s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 45c0 0 125-30 250-30S0 45 0 45s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 105c0 0 125-30 250-30S0 105 0 105s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 15c0 0 125-30 250-30S0 15 0 15s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500-15c0 0 125-30 250-30S0-15 0-15s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 135c0 0 125-30 250-30S0 135 0 135s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3C/g%3E%3C/svg%3E\")" }}>
+        <div style={{ "backgroundColor": "#ffffff", "backgroundImage": "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='250' height='30' viewBox='0 0 1000 120'%3E%3Cg fill='none' stroke='%23fafafa' stroke-width='10' %3E%3Cpath d='M-500 75c0 0 125-30 250-30S0 75 0 75s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 45c0 0 125-30 250-30S0 45 0 45s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 105c0 0 125-30 250-30S0 105 0 105s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 15c0 0 125-30 250-30S0 15 0 15s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500-15c0 0 125-30 250-30S0-15 0-15s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 135c0 0 125-30 250-30S0 135 0 135s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3C/g%3E%3C/svg%3E\")", textAlign: 'center', color: '#1e1e1e', fontSize: '25px' }}>
+          <h5><br></br></h5>
 
-          <h1 className='pt-20 '>OUR TEAM</h1>
+          <h1 className='pt-2 pb-3'>INSTRUCTIONS</h1>
+          <div className='pb-2 mx-3'>
 
-          <div className="menuList">
-            {MenuList.map((menuItem, key) => {
-              return (
-                <MenuItem
-                  key={key}
-                  image={menuItem.image}
-                  name={menuItem.name}
-                  price={menuItem.price}
-                />
-              );
-            })}
+            <h5> Select the Categories from the <Link to='/categories' style={{ textDecoration: 'none' }}>categories</Link> page.<br></br></h5>
+            <h5> There will be 4 choices for each quiz question.<br></br></h5>
+            <h5>Select the correct option.<br></br></h5>
+            <h5>The right answer will be displayed in <span style={{ color: 'green' }}>Green</span> colour.<br></br></h5>
+            <h5>The wrong answers will be displayed in <span style={{ color: 'red' }}>Red</span> colour.</h5>
+            <h5>Click Next to move to the next question.<br></br></h5>
+            <h5>After 10 questions are completed move to leaderboard to see your scores!<br></br></h5>
+            <h5>If you abandon the quiz, your score will be considered as zero.<br></br></h5>
           </div>
 
-          {/* <h1 className='py-16'>Contact us</h1>
+          <div className="menu" style={{ backgroundColor: "#ffffff", paddingBottom: '5rem', "backgroundImage": "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='250' height='30' viewBox='0 0 1000 120'%3E%3Cg fill='none' stroke='%23fafafa' stroke-width='10' %3E%3Cpath d='M-500 75c0 0 125-30 250-30S0 75 0 75s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 45c0 0 125-30 250-30S0 45 0 45s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 105c0 0 125-30 250-30S0 105 0 105s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 15c0 0 125-30 250-30S0 15 0 15s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500-15c0 0 125-30 250-30S0-15 0-15s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 135c0 0 125-30 250-30S0 135 0 135s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3C/g%3E%3C/svg%3E\")" }}>
+
+            <h1 className='pt-20 '>OUR TEAM</h1>
+
+            <div className="menuList">
+              {MenuList.map((menuItem, key) => {
+                return (
+                  <MenuItem
+                    key={key}
+                    image={menuItem.image}
+                    name={menuItem.name}
+                    price={menuItem.price}
+                  />
+                );
+              })}
+            </div>
+
+            {/* <h1 className='py-16'>Contact us</h1>
 
           <div className="align-center items-center">
             <form className="w-full max-w-lg align-center items-center  ">
@@ -111,12 +114,12 @@ function Home() {
             </form>
           </div> */}
 
-          {/* <h5><br></br></h5> */}
-          {/* <h5></h5> */}
-          {/* <h5><br></br></h5> */}
+            {/* <h5><br></br></h5> */}
+            {/* <h5></h5> */}
+            {/* <h5><br></br></h5> */}
 
-        </div>
-        {/* <div className="container mx-auto pb-20 w-3/6 ">
+          </div>
+          {/* <div className="container mx-auto pb-20 w-3/6 ">
           <div className="p-5 space-y-5 shadow-xl ">
             <h1 className="text-center pb-5">Have something to say? Contact Us</h1>
 
@@ -159,11 +162,12 @@ function Home() {
           </div>
         </div> */}
 
+        </div>
+
+        <Footer />
+
       </div>
-
-      <Footer />
-
-    </div>
+    </>
 
   );
 }
